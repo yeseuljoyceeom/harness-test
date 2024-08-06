12f52f4fe894c0cc46c22f82e97a5ea2d997b927
yeseuljoyceeom <yeseuljoyceeom@gamil.com>
2024-08-06 17:35:37 +0900

    initialize repository

---
+++ src/main/java/yeseul/harness_test/HarnessTestApplication.java
@@ -0,0 +1,13 @@
+package yeseul.harness_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarnessTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarnessTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeseul/harness_test/HarnessTestApplicationTests.java
@@ -0,0 +1,13 @@
+package yeseul.harness_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarnessTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
